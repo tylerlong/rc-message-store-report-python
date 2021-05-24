3dc4e0bdbcfdf940e6afa6cf8d824c6f3e6880b7
Tyler Long <tyler4long@gmail.com>
2021-05-24 10:19:36 -0700

    Update code

--- index.py
+++ index.py
@@ -4,19 +4,19 @@ rc = RestClient("clientId", "clientSecret", PRODUCTION_SERVER)
 
 rc.authorize('username', 'extension', 'password')
 
-r = rc.get('/restapi/v1.0/account/000000/message-store-report/111111')
+r = rc.get('/restapi/v1.0/account/~/message-store-report/111111')
 print(r.text)
 
-r = rc.get('/restapi/v1.0/account/000000/message-store-report/111111/archive')
+r = rc.get('/restapi/v1.0/account/~/message-store-report/111111/archive')
 print(r.text)
 
-r = rc.get('/restapi/v1.0/account/000000/message-store-report/111111/archive/0')
+r = rc.get('/restapi/v1.0/account/~/message-store-report/111111/archive/0')
 print(len(r.content))
 file = open("test0.zip", "wb")
 file.write(r.content)
 file.close()
 
-r = rc.get('/restapi/v1.0/account/000000/message-store-report/111111/archive/1')
+r = rc.get('/restapi/v1.0/account/~/message-store-report/111111/archive/1')
 print(len(r.content))
 file = open("test1.zip", "wb")
 file.write(r.content)
